bea8b7e2a5d6d677977e51e03900d1e500fb9a4f
Greg Edwards <edwardsgreg@gmail.com>
2012-04-09 19:22:19 -0400

    Mostly empty gamefloor project

---
+++ Gamefloor/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gamefloor
+{
+    public class Game
+    {
+        public Game()
+        {
+        }
+
+        public void Run()
+        {
+        }
+    }
+}
